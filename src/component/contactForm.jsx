9ab8a53121173bcd43151f7c00a58be4281c160b
mdaftab41 <mdaftabalam@gmail.com>
2023-10-24 11:01:14 +0530

    Revert "form"
    This reverts commit c9314de647038b8437f57e8bf0767ba24449f231.

--- src/component/contactForm.jsx
+++ src/component/contactForm.jsx
@@ -9,38 +9,19 @@ function Form() {
     email: "",
     message: "",
   });
-  //const [filled, setFilled] = useState(false);
-  const [filled, setFilled] = useState({
-    fName: false,
-    lName: false,
-    email: false,
-    message: false,
-  });
+  const [filled, setFilled] = useState(false);
   const [error, setError] = useState({});
 
-  // function handleChange(event) {
-  //   const newObj = { ...contact, [event.target.name]: event.target.value };
-  //   setContact(newObj);
-  //   if (Object.values(newObj).some(value => value.trim() !== '')) {
-  //     setFilled(true);
-  //   } else {
-  //     setFilled(false);
-  //   }
-  // }
-   
-  function handleChange(event, fieldName) {
-    const updatedContact = { ...contact, [fieldName]: event.target.value };
-    setContact(updatedContact);
-
-    // Only update filled state if the input value is not empty
-    if (event.target.value.trim() !== '') {
-      setFilled(prevState => ({ ...prevState, [fieldName]: true }));
+  function handleChange(event) {
+    const newObj = { ...contact, [event.target.name]: event.target.value };
+    setContact(newObj);
+    if (Object.values(newObj).some(value => value.trim() !== '')) {
+      setFilled(true);
     } else {
-      setFilled(prevState => ({ ...prevState, [fieldName]: false }));
+      setFilled(false);
     }
   }
-  
-  
+
   function handleValidation(event) {
     event.preventDefault();
     const validationErrors = Validation(contact);
@@ -56,13 +37,11 @@ function Form() {
   return (
     <div className="pt-5 bg-yellow-50">
       <form className="" onSubmit={handleValidation}>
-        <div className="px-10 py-5 mx-10 my-5 bg-white border border-black rounded-lg shadow-md input-container shadow-black">
+        <div className="px-10 py-5 mx-10 my-5 bg-white border border-black input-container shadow-md shadow-black rounded-lg">
           
-          <label className=   {`relative ${ filled.fName ? "unfilled" : ""}`}>
+          <label className=   {`relative ${filled ? "unfilled" : ""}`}>
             <input
-            onChange={(event) => handleChange(event, "fName")}
-            //onChange={handleChange}
-            value={contact.firstName}
+            onChange={handleChange}
             name="fName"
             title="First Name"
             type="text"
@@ -72,11 +51,10 @@ function Form() {
           {error.fName && <p style={{ color: "red" }}>{error.fName}</p>}
           </label>
         </div>
-        <div className="px-10 py-5 mx-10 my-5 bg-white border border-black rounded-lg shadow-md shadow-black">
-          <label className=   {`relative ${filled.lName ? "unfilled" : ""}`}> 
+        <div className="px-10 py-5 mx-10 my-5 bg-white border border-black shadow-md shadow-black rounded-lg">
+          <label className=   {`relative ${filled ? "unfilled" : ""}`}> 
           <input
-            //onChange={handleChange}
-            onChange={(event) => handleChange(event, "lName")}
+            onChange={handleChange}
             name="lName"
             title="Last Name"
             type="text"
@@ -86,11 +64,10 @@ function Form() {
           {error.lName && <p style={{ color: "red" }}>{error.lName}</p>}
           </label>
         </div>
-        <div className="px-10 py-5 mx-10 my-5 bg-white border border-black rounded-lg shadow-md shadow-black">
-          <label className=   {`relative ${filled.email ? "unfilled" : ""}`}>
+        <div className="px-10 py-5 mx-10 my-5 bg-white border border-black shadow-md shadow-black rounded-lg">
+          <label className=   {`relative ${filled ? "unfilled" : ""}`}>
           <input
-            //onChange={handleChange}
-            onChange={(event) => handleChange(event, "email")}
+            onChange={handleChange}
             name="email"
             title="Email"
             type="text"
@@ -101,15 +78,14 @@ function Form() {
           </div>}
           </label>
         </div>
-        <div className="px-10 py-5 mx-10 my-5 bg-white border border-black rounded-lg shadow-md shadow-black">
-          <label className=   {`relative ${filled.message ? "unfilled" : ""}`}> 
+        <div className="px-10 py-5 mx-10 my-5 bg-white border border-black shadow-md shadow-black rounded-lg">
+          <label className=   {`relative ${filled ? "unfilled" : ""}`}> 
           <input
-            //onChange={handleChange}
-            onChange={(event) => handleChange(event, "massage")}
-            name="message"
+            onChange={handleChange}
+            name="massage"
             title="Write Massage"
             type="text"
-            className="overflow-auto border-b-2 focus:outline-none focus:border-sky-500 w-96 h-fit"
+            className=" border-b-2 focus:outline-none focus:border-sky-500 w-96 overflow-auto h-fit"
           />
           <span className="absolute top-0 left-0 transition duration-200 input-text"> Write Massage</span>
           {error.massage && <p style={{ color: "red" }}>{error.massage}</p>}
