3fd5fa8643a474236e9033ede3b22f211774cced
mdaftab41 <mdaftabalam@gmail.com>
2023-10-25 16:29:58 +0530

    bg change

--- src/component/skill.jsx
+++ src/component/skill.jsx
@@ -5,18 +5,18 @@ import Bar from "./Progress";
 
 function Skill() {
   return (
-    <div className=" bg-cyan-900 bar">
-      <h1 className="text-center border-b-4 border-indigo-600 border-dotted about md:mx-52">
+    <div className=" bg-zinc-950 bar">
+      <h1 className="text-center border-b-4 border-gray-500 border-dotted about md:mx-48">
         Skills
       </h1>
-      <div className="mt-0 text-white mx-80 md:mx-44 about-info">
+      <div className="mx-8 mt-0 text-white md:mx-44 ">
         <p>
           I have a passion for acquiring new skills and keeping up with the
           latest advancements in technology. Currently, I am most proficient in
           using the following technical tools and technologies:
         </p>
       </div>
-      <div className="container grid justify-center w-auto h-auto bg-white rounded-lg md:grid-cols-2 md:justify-between md:h-auto md:mx-36">
+      <div className="container grid justify-center w-auto h-auto mx-4 bg-white rounded-lg md:grid-cols-2 md:justify-between md:h-auto md:mx-36">
         <div className="text-black ">
           <h2 className="my-5 font-bold text-center">I have experience with these technologies</h2>
           <SkillIcon className="" />
