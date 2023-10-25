e415d689703af1324e6f2833f043cd64b435049a
mdaftab41 <mdaftabalam@gmail.com>
2023-10-25 16:28:59 +0530

    Modification in bg and responsive

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,23 +1,31 @@
-import React from "react";
-import Image from "./aftab-Image-1.png";
+import React,{useEffect} from "react";
+import Image from "./Aftab-image2.png";
 import TypeWriter from "./typewriter";
 import data from "./Data";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
  
 
 
 
 function Home(){
+  useEffect(() => {
+    AOS.init();
+});
     return(
-    <div className="px-5 py-32 text-white home-1">
+    <div className="px-5 pt-32 pb-8 text-white home-1">
         <div className="container grid items-center justify-center mx-auto md:grid-cols-2 md:justify-between">
-          <div className="pb-5 hero-info md:pb-0">
-            <h1 className="text-4xl lg:text-6xl ">
+          <div className="pb-5 hero-info md:pb-60 md:pt-0"  >
+            <h1 className="text-4xl lg:text-6xl">
               Hi, I am <TypeWriter texts={data}  className="pt-0 text-accent"/> 
               
             </h1>
-            <p className="py-5">
+            <p className="pt-10 pb-10">
               I am proficient in JavaScript, React.js and Tailwind CSS
             </p>
+            <div  data-aos="zoom-in-right"
+            data-aos-duration="2500"
+             > 
             <a
               href="/#projects"
               className=" btn bg-accent  border-2 border-[#7477FF] text-white px-6 py-3 hover:bg-transparent m-3 rounded-xl"
@@ -30,13 +38,17 @@ function Home(){
             >
               Resume
             </a>
+            </div>
           </div>
   
-          <div className="">
+          <div className="pb-0" data-aos="fade-left"
+          data-aos-easing="linear"
+          data-aos-duration="2500"
+          >
             <img
               src={Image}
               alt="coding illustration"
-              className="lgw-[80%] ml-auto movearrow w-130 h-120 rounded-sm"
+              className="lgw-[80%] ml-auto movearrow w-130 h-120 rounded-sm "
             />
           </div>
         </div>
--- src/component/contactForm.jsx
+++ src/component/contactForm.jsx
@@ -37,7 +37,7 @@ function Form() {
   return (
     <div className="pt-5 bg-yellow-50">
       <form className="" onSubmit={handleValidation}>
-        <div className="px-10 py-5 mx-10 my-5 bg-white border border-black input-container shadow-md shadow-black rounded-lg">
+        <div className="px-10 py-5 mx-10 my-5 bg-white border border-black rounded-lg shadow-md input-container shadow-black">
           
           <label className=   {`relative ${filled ? "unfilled" : ""}`}>
             <input
@@ -45,47 +45,47 @@ function Form() {
             name="fName"
             title="First Name"
             type="text"
-            className="transition duration-200 border-b-2 focus:outline-none focus:border-sky-500 invalid:border-pink-500 w-96"
+            className="transition duration-200 border-b-2 focus:outline-none focus:border-sky-500 invalid:border-pink-500 md:w-96"
           />
           <span className="absolute top-0 left-0 transition duration-200 input-text">First Name</span>
           {error.fName && <p style={{ color: "red" }}>{error.fName}</p>}
           </label>
         </div>
-        <div className="px-10 py-5 mx-10 my-5 bg-white border border-black shadow-md shadow-black rounded-lg">
+        <div className="px-10 py-5 mx-10 my-5 bg-white border border-black rounded-lg shadow-md shadow-black">
           <label className=   {`relative ${filled ? "unfilled" : ""}`}> 
           <input
             onChange={handleChange}
             name="lName"
             title="Last Name"
             type="text"
-            className="border-b-2 focus:outline-none focus:border-sky-500 w-96"
+            className="border-b-2 focus:outline-none focus:border-sky-500 md:w-96"
           />
           <span className="absolute top-0 left-0 transition duration-200 input-text">Last Name</span>
           {error.lName && <p style={{ color: "red" }}>{error.lName}</p>}
           </label>
         </div>
-        <div className="px-10 py-5 mx-10 my-5 bg-white border border-black shadow-md shadow-black rounded-lg">
+        <div className="px-10 py-5 mx-10 my-5 bg-white border border-black rounded-lg shadow-md shadow-black">
           <label className=   {`relative ${filled ? "unfilled" : ""}`}>
           <input
             onChange={handleChange}
             name="email"
             title="Email"
             type="text"
-            className="border-b-2 focus:outline-none focus:border-sky-500 w-96"
+            className="border-b-2 focus:outline-none focus:border-sky-500 md:w-96"
           />
           <span className="absolute top-0 left-0 transition duration-200 input-text"> Email</span>
           {error.email && <div><p style={{ color: "red" }}>{error.email}</p>
           </div>}
           </label>
         </div>
-        <div className="px-10 py-5 mx-10 my-5 bg-white border border-black shadow-md shadow-black rounded-lg">
+        <div className="px-10 py-5 mx-10 my-5 bg-white border border-black rounded-lg shadow-md shadow-black">
           <label className=   {`relative ${filled ? "unfilled" : ""}`}> 
           <input
             onChange={handleChange}
             name="massage"
             title="Write Massage"
             type="text"
-            className=" border-b-2 focus:outline-none focus:border-sky-500 w-96 overflow-auto h-fit"
+            className="overflow-auto border-b-2 focus:outline-none focus:border-sky-500 md:w-96 h-fit"
           />
           <span className="absolute top-0 left-0 transition duration-200 input-text"> Write Massage</span>
           {error.massage && <p style={{ color: "red" }}>{error.massage}</p>}
