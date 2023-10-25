83a8cb5043e19ddc7d765f9beaee07db772a28f5
mdaftab41 <mdaftabalam@gmail.com>
2023-10-26 02:18:04 +0530

    form inpute field

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -12,7 +12,7 @@ function Contact() {
 });
   return (
     <div className="text-white bg-gray-900"> 
-    <h className ="mx-5 text-center border-b-4 border-gray-500 border-dotted about md:mx-40 lg:mx-96 " data-aos="fade-in">Contact Me</h>
+    <h className ="mx-5 text-center border-b-4 border-gray-500 border-dotted about md:mx-40 lg:mx-80 " data-aos="fade-in">Contact Me</h>
     <div className="grid items-center justify-center mx-auto md:grid-cols-2 md:justify-between lg:mx-20">
       <div data-aos="zoom-in">
         <Form  className="text-black"/>
