aa415c2242ef38b8da17039c734a792c2d3e5c54
mdaftab41 <mdaftabalam@gmail.com>
2023-10-28 23:06:06 +0530

    Home

--- src/component/ContactDetail.jsx
+++ src/component/ContactDetail.jsx
@@ -1,15 +1,19 @@
-import React,{useEffect} from "react";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook,     faGithub,faLinkedin,envelope } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faGithub,
+  faLinkedin,
+  envelope,
+} from "@fortawesome/free-brands-svg-icons";
 
 function ContactDetail() {
-   
-    useEffect(() => {
-      AOS.init({
-        duration: 1000
-      });
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
   });
   return (
     <div className="mx-10">
@@ -17,31 +21,43 @@ function ContactDetail() {
         I am currently open for a fulltime Frontend Developer role. If you want
         to discuss about that feel free to email me or call me.
       </h1>
-      <h1 className="font-serif" data-aos="fade-left">  Email:mdaftabalam49041@gmail.com</h1>
+      <h1 className="font-serif" data-aos="fade-left">
+        {" "}
+        Email:mdaftabalam49041@gmail.com
+      </h1>
       <h1 className="ml-12 font-serif"> mh3473@srmist.edu.in</h1>
-      <h1 className="font-serif" data-aos="fade-left">  Mobile Number: 7352205782</h1>
-      <div class="social-container flex justify-around pt-10 md:mx-20" data-aos="zoom-in">
-      <a href="https://www.facebook.com/profile.php?id=100020936580838"
-        className=" facebook social">
-        <FontAwesomeIcon icon={faFacebook} size="2x" />
-    </a>
-      <a href="https://instagram.com/aftab9631?igshid=OGQ5ZDc2ODk2ZA=="
-        className="instagram social insta-icon" id="gram">
-         
-    </a>
-      <a href=" https://github.com/mdaftab41"
-        className="Github social"  >
-        <FontAwesomeIcon icon={ faGithub} size="2x" />
-      </a>
-      <a href="https://www.linkedin.com/in/md-aftab-alam-1bba22237/"
-        className="linkedin social">
-        <FontAwesomeIcon icon={faLinkedin} size="2x" />
-      </a>
+      <h1 className="font-serif" data-aos="fade-left">
+        {" "}
+        Mobile Number: 7352205782
+      </h1>
+      <div
+        class="social-container flex justify-around pt-10 md:mx-20"
+        data-aos="zoom-in"
+      >
+        <a
+          href="https://www.facebook.com/profile.php?id=100020936580838"
+          className=" facebook social"
+        >
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a
+          href="https://instagram.com/aftab9631?igshid=OGQ5ZDc2ODk2ZA=="
+          className="instagram social insta-icon"
+          id="gram"
+        ></a>
+        <a href=" https://github.com/mdaftab41" className="Github social">
+          <FontAwesomeIcon icon={faGithub} size="2x" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/md-aftab-alam-1bba22237/"
+          className="linkedin social"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
       </div>
     </div>
   );
 }
- 
 
 export default ContactDetail;
 //<FontAwesomeIcon icon={faInstagram} size="2x" />
