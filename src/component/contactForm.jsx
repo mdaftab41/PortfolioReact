c410bfff59bd82a867e2472780c65cc0530dce14
mdaftab41 <mdaftabalam49041@gmail.com>
2024-07-06 17:28:41 +0530

    contactForm

--- src/component/contactForm.jsx
+++ src/component/contactForm.jsx
@@ -1,12 +1,7 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Validation from "./Validation";
-import  { useRef } from 'react';
 import emailjs from '@emailjs/browser';
-
- 
-
-
 
 function Form() {
   const [contact, setContact] = useState({
@@ -14,109 +9,97 @@ function Form() {
     email: "",
     message: "",
   });
-  const [filled, setFilled] = useState(false);
+  const [filled, setFilled] = useState({
+    fName: false,
+    email: false,
+    message: false,
+  });
   const [error, setError] = useState({});
 
-   
   function handleChange(event, fieldName) {
     const updatedContact = { ...contact, [fieldName]: event.target.value };
     setContact(updatedContact);
 
-    // Only update filled state if the input value is not empty
     if (event.target.value.trim() !== '') {
       setFilled(prevState => ({ ...prevState, [fieldName]: true }));
     } else {
-      setFilled(false);
       setFilled(prevState => ({ ...prevState, [fieldName]: false }));
     }
   }
 
-  
   function handleValidation(event) {
     event.preventDefault();
     const validationErrors = Validation(contact);
     setError(validationErrors);
 
-    // Check if there are any errors before submitting
-    if (Object.keys(validationErrors).length === 0) {
-      // Perform your submit logic here
-      console.log("Form submitted:", contact);
-    }
+    return Object.keys(validationErrors).length === 0;
   }
 
-  
-  //Email js  code 
-const form = useRef();
+  const form = useRef();
 
-const sendEmail = (e) => {
-  e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-  emailjs
-    .sendForm('service_w3zy7b6', ' template_9bbhio2', form.current, {
-      publicKey: 'he7k4upntRG-ks2PZ',
-    })
-    .then(
-      () => {
-        console.log('SUCCESS!');
-      },
-      (error) => {
-        console.log('FAILED...', error.text);
-      },
-    );
+    if (!handleValidation(e)) {
+      return;
+    }
+
+    emailjs.sendForm('  service_043pogj', ' template_yl8vlqs', form.current, 'he7k4upntRG-ks2PZ ')
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        }
+      );
   };
-
-  
-
-  
 
   return (
     <div className="pt-5 text-black">
-    <form className="" ref={form} onSubmit={(e) => { handleValidation(e); sendEmail(e); }}>
+      <form className="" ref={form} onSubmit={sendEmail}>
         <div className="px-10 py-5 mx-10 my-5 bg-white border border-black rounded-lg shadow-md input-container shadow-black">
-          
-          <label className=   {`relative ${filled.fName ? "unfilled" : ""}`}>
+          <label className={`relative ${filled.fName ? "unfilled" : ""}`}>
             <input
-            //onChange={handleChange}
-            onChange={(event) => handleChange(event, "fName")}
-            name="from_name"
-            title="Full Name"
-            type="text"
-            className="w-full transition duration-200 border-b-2 focus:outline-none focus:border-sky-500 invalid:border-pink-500"
-          />
-          <span className="absolute top-0 left-0 transition duration-200 input-text">Full Name</span>
-          {error.fName && <p style={{ color: "red" }}>{error.fName}</p>}
+              onChange={(event) => handleChange(event, "fName")}
+              name="from_name"
+              title="Full Name"
+              type="text"
+              className="w-full transition duration-200 border-b-2 focus:outline-none focus:border-sky-500 invalid:border-pink-500"
+            />
+            <span className="absolute top-0 left-0 transition duration-200 input-text">Full Name</span>
+            {error.fName && <p style={{ color: "red" }}>{error.fName}</p>}
           </label>
         </div>
-         
+
         <div className="px-10 py-5 mx-10 my-5 bg-white border border-black rounded-lg shadow-md shadow-black">
-          <label className=   {`relative ${filled.email ? "unfilled" : ""}`}>
-          <input
-            //onChange={handleChange}
-            onChange={(event) => handleChange(event, "email")}
-            name="from_email"
-            title="Email"
-            type="text"
-            className="w-full border-b-2 focus:outline-none focus:border-sky-500"
-          />
-          <span className="absolute top-0 left-0 transition duration-200 input-text"> Email</span>
-          {error.email && <div><p style={{ color: "red" }}>{error.email}</p>
-          </div>}
+          <label className={`relative ${filled.email ? "unfilled" : ""}`}>
+            <input
+              onChange={(event) => handleChange(event, "email")}
+              name="from_email"
+              title="Email"
+              type="text"
+              className="w-full border-b-2 focus:outline-none focus:border-sky-500"
+            />
+            <span className="absolute top-0 left-0 transition duration-200 input-text">Email</span>
+            {error.email && <p style={{ color: "red" }}>{error.email}</p>}
           </label>
         </div>
+
         <div className="px-10 py-5 mx-10 my-5 bg-white border border-black rounded-lg shadow-md shadow-black">
-          <label className=   {`relative ${filled.message ? "unfilled" : ""}`}> 
-          <input
-            //onChange={handleChange}
-            onChange={(event) => handleChange(event, "fName")}
-            name="message"
-            title="Write Message"
-            type="text"
-            className="w-full overflow-auto border-b-2 focus:outline-none focus:border-sky-500 h-fit"
-          />
-          <span className="absolute top-0 left-0 transition duration-200 input-text"> Write Message</span>
-          {error.message && <p style={{ color: "red" }}>{error.message}</p>}
+          <label className={`relative ${filled.message ? "unfilled" : ""}`}>
+            <input
+              onChange={(event) => handleChange(event, "message")}
+              name="message"
+              title="Write Message"
+              type="text"
+              className="w-full overflow-auto border-b-2 focus:outline-none focus:border-sky-500 h-fit"
+            />
+            <span className="absolute top-0 left-0 transition duration-200 input-text">Write Message</span>
+            {error.message && <p style={{ color: "red" }}>{error.message}</p>}
           </label>
         </div>
+
         <div className="mx-10">
           <button className="px-4 py-2 mb-1 mr-1 text-lg font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none active:bg-emerald-600 hover:shadow-md focus:outline-none bg-rose-700">
             Submit
@@ -127,5 +110,4 @@ const sendEmail = (e) => {
   );
 }
 
-//value={contact.massage}
 export default Form;
